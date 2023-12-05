9c366e195d681fb23cc6ff0d00fd0b240b19f671
Giedrius <kalniusg@gmail.com>
2023-12-05 14:18:23 +0200

    Initial commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,20 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { ExperienceComponent } from './experience/experience.component';
+import { SkillsComponent } from './skills/skills.component';
+import { ProfileComponent } from './profile/profile.component';
+import { ContactsComponent } from './contacts/contacts.component';
+import { EducationComponent } from './education/education.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ExperienceComponent,
+    SkillsComponent,
+    ProfileComponent,
+    ContactsComponent,
+    EducationComponent
   ],
   imports: [
     BrowserModule
